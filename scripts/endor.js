c39ad77e0a1731cde26f908f103b9aa0a17f7b38
cleancoderocker <scalarocker@googlemail.com>
2016-04-06 12:58:33 +0200

    Added highlighting and scrolling into view of current ToC item

--- scripts/endor.js
+++ scripts/endor.js
@@ -1,100 +1,119 @@
 (function ()
 {
-    "use strict";
-    var slides = [];
-    var active, ctr;
-    init();
-    function keyEvents(e)
+  "use strict";
+  var slides = [];
+  var active, ctr, activeToCItem;
+  init();
+  function keyEvents(e)
+  {
+    var PREV_PAGE = 33;
+    var NEXT_PAGE = 34;
+    var ARROW_LEFT = 37;
+    var ARROW_UP = 38;
+    var ARROW_RIGHT = 39;
+    var ARROW_DOWN = 40;
+    if(e.keyCode === ARROW_RIGHT || e.keyCode === ARROW_DOWN || e.keyCode === NEXT_PAGE)
     {
-        var PREV_PAGE = 33;
-        var NEXT_PAGE = 34;
-        var ARROW_LEFT = 37;
-        var ARROW_UP = 38;
-        var ARROW_RIGHT = 39;
-        var ARROW_DOWN = 40;
-        if(e.keyCode === ARROW_RIGHT || e.keyCode === ARROW_DOWN || e.keyCode === NEXT_PAGE)
-        {
-            location.hash = Math.min(slides.length-1, active+1);
-        }
-        else if(e.keyCode === ARROW_LEFT || e.keyCode === ARROW_UP || e.keyCode === PREV_PAGE)
-        {
-            location.hash = Math.max(0, active-1);
-        }
+      updateSlide(Math.min(slides.length-1, active+1));
     }
-    function hashChange(e)
+    else if(e.keyCode === ARROW_LEFT || e.keyCode === ARROW_UP || e.keyCode === PREV_PAGE)
     {
-        active = checkHash(e.newURL);
-        toggleOpacity();
-        ctr.textContent = active + 1;
-        location.hash = active;
+      updateSlide(Math.max(0, active-1));
     }
-    function load()
+  }
+  function updateSlide(slide) {
+    location.hash = slide;
+    updateToC(slide);
+  }
+
+
+  function updateToC(slide) {
+    if(activeToCItem) {
+      activeToCItem.classList.toggle('active-menu-list-item');
+    }
+    activeToCItem = document.getElementById('menu-list-item-' + slide);
+    activeToCItem.classList.toggle('active-menu-list-item');
+    scrollToCActiveItemIntoView();
+  }
+
+  function scrollToCActiveItemIntoView() {
+    activeToCItem.scrollIntoView();
+  }
+
+  function hashChange(e)
+  {
+    active = checkHash(e.newURL);
+    toggleOpacity();
+    ctr.textContent = active + 1;
+    updateSlide(active);
+  }
+  function load()
+  {
+    active = checkHash(location.toString());
+    slides.forEach(function (slide)
     {
-        active = checkHash(location.toString());
-        slides.forEach(function (slide)
-        {
-            slide.addEventListener("transitionend", toggleDisplay);
-        });
-        toggleOpacity();
-        document.getElementById("slides_total").textContent = slides.length;
-        ctr = document.getElementById("slide_counter");
-        ctr.textContent = active + 1;
-        location.hash = active;
-    }
-    function init()
+      slide.addEventListener("transitionend", toggleDisplay);
+    });
+    toggleOpacity();
+    document.getElementById("slides_total").textContent = slides.length;
+    ctr = document.getElementById("slide_counter");
+    ctr.textContent = active + 1;
+    updateSlide(active);
+  }
+  function init()
+  {
+    slides = Array.prototype.slice.call(
+      document.querySelectorAll("article > section"));
+    slides.unshift(document.querySelectorAll("article > header")[0]);
+    window.onload = load;
+    window.onhashchange = hashChange;
+    window.onkeyup = keyEvents;
+    //window.onkeydown = keyEvents;
+    //window.onkeypress = keyEvents;
+  }
+  function checkHash(url)
+  {
+    var hash = new URL(url).hash;
+    var h = hash ? parseInt(hash.substring(1)) : 0;
+    if(Number.isNaN(h) || h < 0 || h >= slides.length)
     {
-        slides = Array.prototype.slice.call(
-            document.querySelectorAll("article > section"));
-        slides.unshift(document.querySelectorAll("article > header")[0]);
-        window.onload = load;
-        window.onhashchange = hashChange;
-        window.onkeyup = keyEvents;
-        //window.onkeydown = keyEvents;
-        //window.onkeypress = keyEvents;
+      h = 0;
     }
-    function checkHash(url)
+    return h;
+  }
+  function toggleDisplay()
+  {
+    slides.forEach(function (slide, i)
     {
-        var hash = new URL(url).hash;
-        var h = hash ? parseInt(hash.substring(1)) : 0;
-        if(Number.isNaN(h) || h < 0 || h >= slides.length)
-        {
-            h = 0;
-        }
-        return h;
-    }
-    function toggleDisplay()
+      slide.id = i;
+      if(i === active)
+      {
+        slide.style.display = "block";
+        slide.style.removeProperty("opacity");
+        slide.style.removeProperty("transition");
+      }
+      else
+      {
+        slide.style.display = "none";
+      }
+    });
+  }
+  function toggleOpacity()
+  {
+    slides.forEach(function (slide, i)
     {
-        slides.forEach(function (slide, i)
-        {
-            slide.id = i;
-            if(i === active)
-            {
-                slide.style.display = "block";
-                slide.style.removeProperty("opacity");
-                slide.style.removeProperty("transition");
-            }
-            else
-            {
-                slide.style.display = "none";
-            }
-        });
-    }
-    function toggleOpacity()
-    {
-        slides.forEach(function (slide, i)
-        {
-            slide.id = i;
-            if(i === active)
-            {
-                slide.style.opacity = 1;
-            }
-            else
-            {
-                slide.style.opacity = 0;
-                slide.style.transition = "opacity 1s linear";
-            }
-        });
-    }
+      slide.id = i;
+      if(i === active)
+      {
+        slide.style.opacity = 1;
+      }
+      else
+      {
+        slide.style.opacity = 0;
+        slide.style.transition = "opacity 0.5s linear";
+      }
+    });
+  }
 
   document.addEventListener('DOMContentLoaded', addOverlayWithMenu)
 
@@ -104,6 +123,7 @@
     overlay.id = 'overlay';
     function openOverlayMenu() {
       overlay.classList.add('active-menu');
+      scrollToCActiveItemIntoView();
     }
     function closeOverlayMenu() {
       overlay.classList.remove('active-menu');
@@ -137,6 +157,7 @@
     {
       var listItem = document.createElement('li');
       listItem.classList.add('menu-list-item');
+      listItem.id = 'menu-list-item-' + (index + 1);
       var linkText = '';
       var mainHeadline = slide.querySelector('h1');
       if(mainHeadline) {
